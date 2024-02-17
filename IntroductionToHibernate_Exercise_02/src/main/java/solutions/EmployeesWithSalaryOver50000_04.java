22cc6118bb7a65205547834774517c84e435cb22
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-02-17 20:18:45 +0100

    Update EmployeesWithSalaryOver50000_04.java. 'entityManager.getTransaction().begin();' Removed!

--- IntroductionToHibernate_Exercise_02/src/main/java/solutions/EmployeesWithSalaryOver50000_04.java
+++ IntroductionToHibernate_Exercise_02/src/main/java/solutions/EmployeesWithSalaryOver50000_04.java
@@ -9,7 +9,7 @@ public class EmployeesWithSalaryOver50000_04 {
     public static void main(String[] args) {
         EntityManager entityManager = Utils.getEntityManager();
 
-        entityManager.getTransaction().begin();
+        
 
         List<String> resultList = entityManager.createQuery("SELECT e.firstName FROM Employee e" +
                 " WHERE e.salary > 50000", String.class).getResultList();
