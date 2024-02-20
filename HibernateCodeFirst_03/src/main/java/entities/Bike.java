b1d104c91b0965a0ebe5471f08c2c51b391bb04a
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-02-20 11:17:00 +0100

    Change inheritance strategy to SINGLE_TABLE

--- HibernateCodeFirst_03/src/main/java/entities/Bike.java
+++ HibernateCodeFirst_03/src/main/java/entities/Bike.java
@@ -1,10 +1,12 @@
 package entities;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "bikes")
+@DiscriminatorValue("bike")
 public class Bike extends Vehicle{
 
     private int gearsCount;
