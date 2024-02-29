0f973ba8a606a3aa9e1c93e82b955aadf66f6b18
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-02-29 01:10:51 +0100

    Query 2 - 2.	Get all authors with at least one book with a release date before 1990. Print their first name and last name.

--- SpringDataExercise_06/main/java/com/example/springdataexercise/ConsoleRunner.java
+++ SpringDataExercise_06/main/java/com/example/springdataexercise/ConsoleRunner.java
@@ -1,6 +1,8 @@
 package com.example.springdataexercise;
 
+import com.example.springdataexercise.entities.Author;
 import com.example.springdataexercise.entities.Book;
+import com.example.springdataexercise.repositories.AuthorRepository;
 import com.example.springdataexercise.repositories.BookRepository;
 import com.example.springdataexercise.services.SeedService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,11 +16,13 @@ import java.util.List;
 public class ConsoleRunner implements CommandLineRunner {
     private final SeedService seedService;
     private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
 
     @Autowired
-    public ConsoleRunner(SeedService seedService, BookRepository bookRepository) {
+    public ConsoleRunner(SeedService seedService, BookRepository bookRepository, AuthorRepository authorRepository) {
         this.seedService = seedService;
         this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
     }
 
     @Override
@@ -27,7 +31,8 @@ public class ConsoleRunner implements CommandLineRunner {
         // this.seedService.seedCategories();
 
         //this.seedService.seedAll();
-        _01_booksAfter2000();
+        //_01_booksAfter2000();
+        _02_authorsWithBooksBefore1990();
 
     }
     private void _01_booksAfter2000(){
@@ -36,5 +41,12 @@ public class ConsoleRunner implements CommandLineRunner {
         List<Book> books = this.bookRepository.findByReleaseDateAfter(year2000);
 
         books.forEach(b-> System.out.println(b.getReleaseDate() + " " + b.getTitle()));
+    }
+    private void _02_authorsWithBooksBefore1990(){
+        LocalDate year1990 = LocalDate.of(1990, 1, 1);
+
+        List<Author> authors = this.authorRepository.findDistinctByBooksReleaseDateBefore(year1990);
+
+        authors.forEach(a-> System.out.println(a.getFirstName() + " " + a.getLastName()));
     }
 }
