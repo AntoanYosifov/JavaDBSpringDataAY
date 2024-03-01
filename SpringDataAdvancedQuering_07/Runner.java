479a97a53bfc8e1d0c157c6172b4b39c30286096
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-03-01 02:48:56 +0100

    Advanced Querying initial commit.

---
+++ SpringDataAdvancedQuering_07/Runner.java
@@ -0,0 +1,22 @@
+package com.example.advquerying;
+
+import com.example.advquerying.repositories.ShampooRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+    private final ShampooRepository shampooRepository;
+
+    @Autowired
+    public Runner(ShampooRepository shampooRepository) {
+        this.shampooRepository = shampooRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.shampooRepository.findAllByBrand("Cotton Fresh")
+                .forEach(s -> System.out.println(s.getId()));
+    }
+}
